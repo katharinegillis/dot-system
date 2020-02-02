193ff0f5d5afc806409a3e2f3a6203f9f101a00b
Katharine Cordes <gillis.katharine@gmail.com>
2020-02-02 10:52:32 -0700

    Add description echos

--- ellipsis.sh
+++ ellipsis.sh
@@ -5,6 +5,10 @@ pkg.install() {
 }
 
 pkg.pull() {
+    echo -e "\e[32mUpdating system...\e[0m"
+
     # Update all installed packages and package repositories
     sudo apt-get update && sudo apt-get upgrade
+
+    echo -e "\e[32mDone system.\e[0m"
 }
