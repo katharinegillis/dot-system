86266e6f4fa7102a07aaaba55420815ab81a03a9
Katharine Cordes <gillis.katharine@gmail.com>
2020-04-28 19:40:19 -0700

    Fix pull to get git changes

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 pkg.install() {
-    pkg.pull
+    # Update the OS packages
+    updateOSPackages
 }
 
 pkg.pull() {
-    echo -e "\e[32mUpdating system...\e[0m"
+    # Pull down package updates
+    git.pull
 
+    # Update the OS packages
+    updateOSPackages
+}
+
+updateOSPackages() {
     # Update all installed packages and package repositories
     if utils.cmd_exists apt-get; then
         sudo apt-get -y update && sudo apt-get -y upgrade
     fi
-
-    echo -e "\e[32mDone system.\e[0m"
 }
