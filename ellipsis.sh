5350fdab11e23aea51b7356d176d9e67e5ec61a1
Katharine Cordes <gillis.katharine@gmail.com>
2020-04-28 21:36:59 -0700

    Fix pull to only occur when there are changes

--- ellipsis.sh
+++ ellipsis.sh
@@ -6,8 +6,12 @@ pkg.install() {
 }
 
 pkg.pull() {
-    # Pull down package updates
-    git.pull
+    # Check for updates on git
+    git remote update 2>&1 > /dev/null
+    if git.is_behind; then
+        # Pull down package updates
+        git.pull
+    fi
 
     # Update the OS packages
     bash $PKG_PATH/run.sh "$ELLIPSIS_SRC"
