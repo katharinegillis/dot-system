2890fb55e7d6dd11e51398f81eb36f7c94d1b417
Katharine Cordes <gillis.katharine@gmail.com>
2021-07-18 16:14:25 -0600

    feat: add keys to agent automatically

--- install.sh
+++ install.sh
@@ -10,3 +10,15 @@ else
     sudo apt-get -y update
     sudo apt-get -y upgrade
 fi
+
+if [ ! -f "$HOME/.ssh/config" ]; then
+    touch "$HOME/.ssh/config"
+    chmod 0600 "$HOME/.ssh/config"
+fi
+
+if ! grep -Fxq "AddKeysToAgent yes" "$HOME/.ssh/config"; then
+    {
+        echo ""
+        echo "AddKeysToAgent yes"
+    } >> "$HOME/.ssh/config"
+fi
